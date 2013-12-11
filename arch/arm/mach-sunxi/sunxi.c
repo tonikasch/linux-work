b4df7a10ccfb92203e4917dfac414970721f0913
Andrey Panov <rockford@yandex.ru>
2013-12-11 12:36:38 +0400

    sunxi.c: Comment out sun6i_timer_init

--- arch/arm/mach-sunxi/sunxi.c
+++ arch/arm/mach-sunxi/sunxi.c
@@ -146,7 +146,7 @@ static void __init sun6i_timer_init(void)
 
 DT_MACHINE_START(SUN6I_DT, "Allwinner sun6i (A31) Family")
 	.init_machine	= sunxi_dt_init,
-	.init_time	= sun6i_timer_init,
+//	.init_time	= sun6i_timer_init,
 	.dt_compat	= sun6i_board_dt_compat,
 	.restart	= sun6i_restart,
 MACHINE_END
